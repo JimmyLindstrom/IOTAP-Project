7c0887608e8104a91aae375e41bd9a44a942945d
JimmyLindstrom <jimmylindstrom79@gmail.com>
2017-01-20 12:50:42 +0100

    small changes to index and sockets file

--- sockets.py
+++ sockets.py
@@ -8,10 +8,9 @@ import serial
 import sys
 import os
 
-try:
-    import ibmiotf.device
-except ImportError:
-    import ibmiotf.device
+import ibmiotf.device
+import ibmiotf.application
+
 
 
 app = Flask(__name__)
@@ -35,7 +34,7 @@ class Arduino (threading.Thread):
             if "Arduino" in p[1]:
                 try:
                     self.arduino = serial.Serial(p[0], 9600, timeout=10)
-                    print_out("Connected to " + p[0])
+                    print("Connected to " + p[0])
                     break
                 except serial.SerialException:
                     print("Couldn't connect to " + p[0])
@@ -43,7 +42,7 @@ class Arduino (threading.Thread):
         if self.arduino is not None:
             self.arduino.close()  # In case the port is already open this closes it.
             self.arduino.open()  # Reopen the port.
-            print_out('Arduino thread started')
+            print('Arduino thread started')
             while self.running:
                 if stop_running.is_set():
                     self.arduino.close()
@@ -54,17 +53,17 @@ class Arduino (threading.Thread):
                     data = self.arduino.readline()[:-2]
                 data = str(data)
                 if data == "Receive" and not receive_movement.is_set():
-                    print_out("Start receiving movements")
+                    print("Start receiving movements")
                     receive_movement.set()
                     with app.test_request_context('/'):
-                        print_out('user connected')
-                        socketio.emit('news', 'user connected')
+                        print('user connected')
+                        socketio.emit('my response', 'user connected')
 
                 elif data == "Stop" and receive_movement.is_set():
                     print("Stop receiving movements!")
                     receive_movement.clear()
                     user_connect_sent.clear()
-            print_out('EXITING ARDUINO THREAD')
+            print('EXITING ARDUINO THREAD')
 
 """ -----------------------------------------------
  Class that handles IBM Bluemix connection
@@ -89,9 +88,8 @@ class Bluemix (threading.Thread):
                 with app.test_request_context('/'):
                     socketio.emit('news', data['movement'])
 
-        # Initialize the device deviceCli.
+        # Initialize the device deviceCli. Fetches credentials from config.txt.
         try:
-            # deviceCliOptions = {"org": organization, "type": deviceType, "id": deviceId, "auth-method": authMethod, "auth-token": authToken}
             path = os.path.join(sys.path[0], 'config.txt')
             deviceCliOptions = ibmiotf.device.ParseConfigFile(path)
             deviceCli = ibmiotf.device.Client(deviceCliOptions)
@@ -103,9 +101,10 @@ class Bluemix (threading.Thread):
         deviceCli.connect()
         deviceCli.commandCallback = myCommandCallback
 
-        user = "123aa123aa12" # DeviceId of the smart wristband in proximity
+        # User credentials hardcodeded, shold be received by user in proximity
+        # in the future!!
+        user = "123aa123aa12"
 
-        counter = 0;
         while self.running:
             if stop_running.is_set():
                 self.running = False
@@ -115,21 +114,18 @@ class Bluemix (threading.Thread):
                 if receive_movement.is_set():
                     if not user_connect_sent.is_set():
                         timestamp = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-                        print(timestamp)
                         data = { 'user' : user, 'timestamp' : timestamp}
                         def myOnPublishCallback():
-                            print("Confirmed event " + str(counter) +" received by IoTF\n" )
+                            print("Confirmed event received by IoTF\n" )
                         
                         success = deviceCli.publishEvent("user", "json", data, qos=0, on_publish=myOnPublishCallback)
+                        user_connect_sent.set()
                         if not success:
                             print("Not connected to IoTF")
-                        user_connect_sent.set()
 
         print('EXITING BLUEMIX THREAD')
 
 
-thread = None
-thread_2 = None
 """  Events used to control the threads """
 stop_running = threading.Event() # event if threads should stop running
 receive_movement = threading.Event() # event if we should receive rmovements
@@ -140,33 +136,14 @@ user_connect_sent = threading.Event()  # event if user connect is sent to bluemi
 def index():
     return render_template('index.html')
 
+
 @socketio.on('connect')
 def handle_connect():
-    print('a user connected ')
-    emit('my response', {'data': 'connected'})
-       
-    
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('a user disconnected ')
-
-@socketio.on('exit')
-def handle_exit(msg):
-    print("Closing threads and server!")
-    close_threads()
-    socketio.stop()
-
-def send_message(msg) :
-    emit('news', msg)
-
-def print_out(msg):
-    print(msg)
-
-def close_threads():
-    print('In close threads!!!')
-    stop_running.set()
-
-if __name__ == '__main__':
+    global connected_users
+    global thread
+    global thread_2
+    connected_users = connected_users + 1
+    print('user %s is connected!' %(connected_users))
     if thread == None:
         thread = Arduino()
         thread.deamon = True
@@ -175,4 +152,50 @@ if __name__ == '__main__':
         thread_2 = Bluemix()
         thread_2.deamon = True
         thread_2.start()
+    emit('my response', {'data': 'connected'})
+       
+    
+@socketio.on('disconnect')
+def handle_disconnect():
+    print("IN DISCONNECT!!!!!!!")
+    global connected_users
+    print('user %s is disconnected!' %(connected_users))
+    connected_users = connected_users -1
+    if connected_users < 1:
+        close_threads()
+
+
+@socketio.on('exit')
+def handle_exit(msg):
+    print("Closing threads and server!")
+    close_threads()
+    socketio.stop()
+
+
+def send_message(msg) :
+    emit('news', msg)
+
+
+def close_threads():
+    print('In close threads!!!')
+    stop_running.set()
+    global thread
+    global thread_2
+    thread = None
+    thread_2 = None
+
+
+if __name__ == '__main__':
+    thread = None #Arduino thread!
+    thread_2 = None # Bluemix thread
+    connected_users = 0
+    # if thread == None:
+    #     thread = 
+    #     thread.deamon = True
+    #     thread.start()
+    # if thread_2 == None:
+    #     thread_2 = Bluemix()
+    #     thread_2.deamon = True
+    #     thread_2.start()
     socketio.run(app)
+
